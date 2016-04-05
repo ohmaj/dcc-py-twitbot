7bc76e992dd60b213eabb75be7a80a313765fe9b
Joel Ohman <Joel Ohman>
2016-04-05 08:46:16 -0600

    needs soemthign fixed with api ststus string still

--- Twit.py
+++ Twit.py
@@ -7,7 +7,7 @@ argfile = str("liners.txt")
 
  
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY =  'hovfJHtjxVr6CNlXiSa3gtRN'#keep the quotes, replace this with your consumer key
+CONSUMER_KEY = 'hovfJHtjxVr6CNlXiSa3gtRN'#keep the quotes, replace this with your consumer key
 CONSUMER_SECRET = 'rvlnIPV0ETkeABcxtx5BPUZV7fvDGN4NVsn7udyB4PfLmzG7e9'#keep the quotes, replace this with your consumer secret key
 ACCESS_KEY = '4899243647-LpMjmHaLQDMUytwyIszG8PTHk7VGk81jeztPD2K'#keep the quotes, replace this with your access token
 ACCESS_SECRET = 'V549JWkABgjxZlyWQytgf1bkWoTmn5LSC9jg2D0t0VhS6'#keep the quotes, replace this with your access token secret
@@ -23,6 +23,6 @@ while True:
     for line in f:
         curtime= time.strftime("%H:%M:%S")
         curdate= time.strftime("%x")
-        api.update_status("{0}---{1}, {2}".format(curdate,curtime,line)) 
+        api.update_status(status="{0}---{1}, {2}".format(curdate,curtime,line)) 
         TimeToSleep = randint(780,1200)
         time.sleep(TimeToSleep)#Tweet every 15 minutes
