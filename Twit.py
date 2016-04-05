cf50d8ad7a91af1e1b575dcbeaed7274f85519fe
Joel Ohman <Joel Ohman>
2016-04-04 20:34:35 -0600

    added API keys

--- Twit.py
+++ Twit.py
@@ -7,10 +7,10 @@ argfile = str("liners.txt")
 
  
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY =  'key'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = 'key'#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = 'key'#keep the quotes, replace this with your access token
-ACCESS_SECRET = 'key'#keep the quotes, replace this with your access token secret
+CONSUMER_KEY =  'hovfJHtjxVr6CNlXiSa3gtRN'#keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = 'rvlnIPV0ETkeABcxtx5BPUZV7fvDGN4NVsn7udyB4PfLmzG7e9'#keep the quotes, replace this with your consumer secret key
+ACCESS_KEY = '4899243647-LpMjmHaLQDMUytwyIszG8PTHk7VGk81jeztPD2K'#keep the quotes, replace this with your access token
+ACCESS_SECRET = 'V549JWkABgjxZlyWQytgf1bkWoTmn5LSC9jg2D0t0VhS6'#keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
--- replykeys.py
+++ replykeys.py
@@ -1,10 +1,10 @@
 
 #visit http://dev.twitter.com to create an application and get your keys
 keys = dict(
-consumer_key =          'key',
-consumer_secret =       'key', 
-access_token =          'key',
-access_token_secret =   'key',
+consumer_key =          'hovfJHtjxVr6CNlXiSa3gtRNj',
+consumer_secret =       'rvlnIPV0ETkeABcxtx5BPUZV7fvDGN4NVsn7udyB4PfLmzG7e9', 
+access_token =          '4899243647-LpMjmHaLQDMUytwyIszG8PTHk7VGk81jeztPD2K',
+access_token_secret =   'V549JWkABgjxZlyWQytgf1bkWoTmn5LSC9jg2D0t0VhS6',
  
  
 )
